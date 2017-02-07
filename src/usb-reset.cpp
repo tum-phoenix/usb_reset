6f4b82df01eaf8105c8206ffffe2762797158cd2
Hans Kirchner <hans-kirchner@gmx.de>
2017-02-07 13:35:24 +0100

    Alternative reset via ioctl

--- src/usb-reset.cpp
+++ src/usb-reset.cpp
@@ -2,6 +2,11 @@
 #include <sstream>
 #include <iostream>
 #include <libusb-1.0/libusb.h>
+#include <fcntl.h>
+#include <linux/usbdevice_fs.h>
+#include <unistd.h>
+#include <cstdio>
+#include <sys/ioctl.h>
 
 int reset(uint16_t vendorId, uint16_t productId) {
     libusb_context *context = NULL;
@@ -28,38 +33,64 @@ int reset(uint16_t vendorId, uint16_t productId) {
     return rc;
 }
 
+int resetByFilename(const char* filename) {
+    int fd = open(filename, O_WRONLY);
+    if (fd < 0) {
+        perror("Error opening output file");
+        return false;
+    }
+
+    int rc = ioctl(fd, USBDEVFS_RESET, 0);
+    if (rc < 0) {
+        perror("Error in ioctl");
+        return false;
+    }
+
+    std::cout << "Reset successful" << std::endl;
+
+    close(fd);
+
+    return true;
+}
+
 int main(int argc, char** argv) {
-    if(argc != 3) {
+    if(argc != 3 && argc != 2) {
         std::cerr << "Usage: " << argv[0] << " VID PID" << std::endl;
         std::cerr << "  Example: " << argv[0] << " 0xDEAD 0xBEEF" << std::endl;
+        std::cerr << std::endl;
+        std::cerr << "Alternative usage: " << argv[0] << " FILENAME" << std::endl;
         return EXIT_FAILURE;
     }
 
-    uint16_t vendorId  = 0;
-    uint16_t productId = 0;
+    if(argc == 3) {
+        uint16_t vendorId  = 0;
+        uint16_t productId = 0;
 
-    // Parse inputs
-    {
-        std::string vid(argv[1]);
-        std::istringstream vidStream(vid);
-        vidStream >> std::hex >> vendorId;
+        // Parse inputs
+        {
+            std::string vid(argv[1]);
+            std::istringstream vidStream(vid);
+            vidStream >> std::hex >> vendorId;
 
-        std::string pid(argv[2]);
-        std::istringstream pidStream(pid);
-        pidStream >> std::hex >> productId;
-    }
+            std::string pid(argv[2]);
+            std::istringstream pidStream(pid);
+            pidStream >> std::hex >> productId;
+        }
 
-    if( !vendorId || !productId ) {
-        std::cerr << "Invalid vendor or product id!" << std::endl;
-        return EXIT_FAILURE;
-    }
+        if( !vendorId || !productId ) {
+            std::cerr << "Invalid vendor or product id!" << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    std::cout << std::hex << std::showbase << vendorId << " " << productId << std::endl;
+        std::cout << std::hex << std::showbase << vendorId << " " << productId << std::endl;
 
-    int ret = reset(vendorId, productId);
-    if(ret == 0) {
-        return EXIT_SUCCESS;
-    } else {
-        return ret;
+        int ret = reset(vendorId, productId);
+        if(ret == 0) {
+            return EXIT_SUCCESS;
+        } else {
+            return ret;
+        }
+    } else if(argc == 2) {
+        return resetByFilename(argv[1]) ? EXIT_SUCCESS : EXIT_FAILURE;
     }
 }
